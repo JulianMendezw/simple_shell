0d7cafbf8208f6b3f67e556d3845675282ca4341
Julian <julianlx@hotmail.com>
2020-11-22 22:36:28 -0500

    updating minishell

--- minishell.c
+++ minishell.c
@@ -10,10 +10,8 @@
 
 /**
  * main - super minishel ♥.
- *
  * @ac: arguments count.
  * @argv: arguments.
- *
  * Return: Always return 0.
  */
 
@@ -36,7 +34,6 @@ int main(int ac, char *argv[])
 		{
 			buffer[r_bytes - 1] = '\0';
 			array = _split_string(buffer, " ");
-
 			if (buffer[0] == '/' || buffer[0] == ' ')
 			{
 				token = strtok(buffer, " "); /*By Jong line ♥*/
@@ -45,14 +42,12 @@ int main(int ac, char *argv[])
 			}
 			else
 				token = _string_directory(array);
-
 			if (_strcmp(array[0], xclose[0]) == 0) /* By sotelo line ♥ */
 				free(array[0]), free(array), free(token), free(buffer),	exit(0);
-
 			if (_print_env(array) != 1)
 			{
 				if (stat(token, &st) == 0)
-					_exec(token, array);
+				_exec(token, array);
 			}
 		}
 		if (isatty(STDIN_FILENO) != 0)
