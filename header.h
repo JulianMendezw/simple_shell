0e8010773140aa3b4f6f516f9ab05ca7787b2369
Julian <julianlx@hotmail.com>
2020-11-22 23:39:35 -0500

    updating header

--- header.h
+++ header.h
@@ -11,5 +11,7 @@ int _print_env(char **array);
 int _exec(char *token, char *array[]);
 char *_strdup(char *str);
 int _strcmp(char *s1, char *s2);
+void double_free(char **i_want_to_be_free);
+char *_token(char *buffer, char **array);
 
 #endif
