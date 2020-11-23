dae23d7f8ce4617f47a33209ff8881e26e457e50
Julian <julianlx@hotmail.com>
2020-11-22 23:35:56 -0500

    new function _token

--- minishell.c
+++ minishell.c
@@ -17,8 +17,8 @@
 
 int main(int ac, char *argv[])
 {
-	int r_bytes = 0;
 	size_t nbytes = 0;
+	int r_bytes = 0;
 	char *buffer = NULL, **array = NULL, *token = NULL;
 	char *xclose[] = {"exit", NULL};
 	struct stat st;
@@ -34,21 +34,12 @@ int main(int ac, char *argv[])
 		{
 			buffer[r_bytes - 1] = '\0';
 			array = _split_string(buffer, " ");
-			if (buffer[0] == '/' || buffer[0] == ' ')
-			{
-				token = strtok(buffer, " "); /*By Jong line ♥*/
-				if (token[0] != '/')
-				token = _string_directory(&token);
-			}
-			else
-				token = _string_directory(array);
+			token = _token(buffer, array);
+
 			if (_strcmp(array[0], xclose[0]) == 0) /* By sotelo line ♥ */
 				free(array[0]), free(array), free(token), free(buffer),	exit(0);
 			if (_print_env(array) != 1)
-			{
-				if (stat(token, &st) == 0)
-				_exec(token, array);
-			}
+				if (stat(token, &st) == 0) _exec(token, array);
 		}
 		if (isatty(STDIN_FILENO) != 0)
 		write(STDOUT_FILENO, "\033[94mminishell$: \033[0m", 16);
