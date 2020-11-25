c520fd7670a52a81d6fb14023678c4f8f6e5b9b7
Julian <julianlx@hotmail.com>
2020-11-25 01:00:04 -0500

    updating header with group of functions

--- header.h
+++ header.h
@@ -18,20 +18,27 @@
 #include <sys/wait.h>
 
 extern char **environ;
-char **_split_string(char *str, char *delim);
-void _ctrl_c(int sig);
-char *_strcat(char *dest, char *src);
-char *_string_directory(char **argv);
-char *_getenv(char *name);
-int _print_env(char **array);
-int _exec(int count_w, char *token, char *array[]);
+
+/* string */
+char *_token(char *buffer, char **array);
 char *_strdup(char *str);
 int _strcmp(char *s1, char *s2);
-void _sfree(char **i_want_to_be_free);
-char *_token(char *buffer, char **array);
+
+/* aux */
+char **_split_string(char *str, char *delim);
+char *_string_directory(char **argv);
+char *_strtok(char *str, const char *delim);
+
+/* exec */
+int _exec(int count_w, char *token, char *array[]);
+char *_getenv(char *name);
+void _ctrl_c(int sig);
+int _print_env(char **array);
+
+/* final */
 void _error(int count_w, char *array[]);
 char *_itoa(int integer);
-char *_strtok(char *str, const char *delim);
+void _sfree(char **i_want_to_be_free);
 char *_hack_path(char *path);
 
 #endif
