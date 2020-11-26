d121bb94d11cc337858d5ff2b692faa1261b6af2
Julian <julianlx@hotmail.com>
2020-11-25 19:45:15 -0500

    deleting _free(array)

--- exec.c
+++ exec.c
@@ -19,13 +19,12 @@ int _exec(int count_w, char *token, char *array[])
 	{
 		child = fork();
 		if (child == -1)
-			free(token), _sfree(array);
+			free(token),
 			perror("Error"), exit(1);
 
 		if (child == 0)
 		{
 			if (execve(token, array, NULL) == -1)
-				_sfree(array), free(token),
 				perror("Error Execve"), exit(1);
 		}
 		else
